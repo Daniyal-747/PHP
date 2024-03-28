c926c8c360e7798b591a8517bed2e2d23815c0e9
Daniyal Mirza <daniyal.aptech96@gmail.com>
2024-03-28 17:21:19 +0500

    Table Create

--- create_read.php
+++ create_read.php
@@ -6,9 +6,18 @@
  ?>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<body>
+    <style>
+        body{
+            background-color: black;
+        }
+    </style>
+</body>
+
 <div class="container">
-    <table class="table">
-    <tr>
+    <table class="table table-bordered text-white text-center">
+    <tr class="bg-primary">
                 <th>ID</th>
                 <th>Name</th>
                 <th>Email</th>
@@ -18,12 +27,14 @@
             </tr>
         
             <?php
-            while ($row = mysqli_fetch_assoc($result)) {
+            while ($rows = mysqli_fetch_assoc($result)) {
                 echo "<tr>
-                 <td>". $row['id']. "</td>
-                 <td>". $row['name']. "</td>
-                 <td>". $row['email']. "</td>
-                 <td>". $row['password']. "</td>
+                 <td>".$rows['id']."</td>
+                 <td>".$rows['name']."</td>
+                 <td>".$rows['email']."</td>
+                 <td>".$rows['password']."</td>
+                 <td><a href='c_update.php?id=".$rows['id']."'class='btn btn-success'>Edit</a></td>
+                 <td><a href='d_delete.php?id=".$rows['id']."'class='btn btn-danger'>Delete</a></td>
                  </tr>";
             }
            ?>
